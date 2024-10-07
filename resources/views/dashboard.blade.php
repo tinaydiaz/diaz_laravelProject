351992541a5195c2f3d0bd9f4c867befb2146f6c
tinaydiaz <vdiazchristinama0315@gmailo.com>
2024-10-07 18:43:26 +0800

    text

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,8 +8,11 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <h3 class= "text-red-700 font-bolder"> Welcome Tina!</h3>
+
+                    <p class="bg-blue-400 p-8 rounded-lg text-white">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum facere repudiandae quae, distinctio ipsum nobis assumenda eos fugiat molestiae ipsam?</p>
                 </div>
             </div>
         </div>
